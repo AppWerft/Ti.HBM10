555cd822cef9bd1499384b233a121477c3037aea
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-09-19 18:09:19 +0200

    removing of endpoint, now host + port

--- android/src/ti/airlino/AirlinoDevice.java
+++ android/src/ti/airlino/AirlinoDevice.java
@@ -4,23 +4,6 @@ import java.net.InetAddress;
 import android.net.nsd.NsdServiceInfo;
 
 public class AirlinoDevice {
-	private String endpoint;
-
-	/**
-	 * @return the endpoint
-	 */
-	public String getEndpoint() {
-		return endpoint;
-	}
-
-	/**
-	 * @param endpoint
-	 *            the endpoint to set
-	 */
-	public void setEndpoint(String endpoint) {
-		this.endpoint = endpoint;
-	}
-
 	private String host;
 
 	public String getHost() {
@@ -51,17 +34,13 @@ public class AirlinoDevice {
 	private int port;
 
 	public static AirlinoDevice parseNsdServiceInfo(NsdServiceInfo so) {
-		String endpoint = "http://";
 		AirlinoDevice device = new AirlinoDevice();
 		InetAddress address = so.getHost();
 		if (address != null) {
 			device.setHost(address.getHostAddress());
-			endpoint += address.getHostAddress();
 		}
 		device.setPort(so.getPort());
-		endpoint += (":" + so.getPort() + "/api/v15/radio.action");
 		device.setName(so.getServiceName());
-		device.setEndpoint(endpoint);
 		return device;
 	}
 
