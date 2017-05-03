ca9ad905b27a5a0e96368e53d29b1439ee799fc5
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-05-03 18:34:36 +0200

    options for msearch added

--- android/src/ti/airlino/MSearchSDDPRequest.java
+++ android/src/ti/airlino/MSearchSDDPRequest.java
@@ -6,31 +6,26 @@ import java.net.*;
 public class MSearchSDDPRequest {
 
 	/*
-	 * multicast SSDP M-SEARCH example for finding the IP Address of a Roku
-	 * device. For more info go to:
-	 * http://sdkdocs.roku.com/display/sdkdoc/External+Control+Guide
+	 * multicast SSDP M-SEARCH
 	 */
 
-	public MSearchSDDPRequest() throws IOException {
-
+	public static void mSearch(String host, int port) throws IOException {
 		/* create byte arrays to hold our send and response data */
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
-
 		/* our M-SEARCH data as a byte array */
-		String MSEARCH = "M-SEARCH * HTTP/1.1\nHost: 239.255.255.250:1900\nMan: \"ssdp:discover\"\nST: roku:ecp\n";
+		String MSEARCH = "M-SEARCH * HTTP/1.1\nHost: " + host + ":" + port
+				+ "\nMan: \"ssdp:discover\"\nST: roku:ecp\n";
 		sendData = MSEARCH.getBytes();
-
 		/* create a packet from our data destined for 239.255.255.250:1900 */
 		DatagramPacket sendPacket = null;
 		try {
 			sendPacket = new DatagramPacket(sendData, sendData.length,
-					InetAddress.getByName("239.255.255.250"), 1900);
+					InetAddress.getByName(host), port);
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 		/* send packet to the socket we're creating */
 		DatagramSocket clientSocket = null;
 		try {
@@ -40,18 +35,14 @@ public class MSearchSDDPRequest {
 			e.printStackTrace();
 		}
 		clientSocket.send(sendPacket);
-
 		/* recieve response and store in our receivePacket */
 		DatagramPacket receivePacket = new DatagramPacket(receiveData,
 				receiveData.length);
 		clientSocket.receive(receivePacket);
-
 		/* get the response as a string */
 		String response = new String(receivePacket.getData());
-
 		/* print the response */
 		System.out.println(response);
-
 		/* close the socket */
 		clientSocket.close();
 	}
