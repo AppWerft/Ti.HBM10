23ee6d1c83f7f0e3137eb8f13a362ca075923c2a
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-05-03 18:31:07 +0200

    M-Read added

---
+++ android/src/ti/airlino/MSearchSDDPRequest.java
@@ -0,0 +1,58 @@
+package ti.airlino;
+
+import java.io.*;
+import java.net.*;
+
+public class MSearchSDDPRequest {
+
+	/*
+	 * multicast SSDP M-SEARCH example for finding the IP Address of a Roku
+	 * device. For more info go to:
+	 * http://sdkdocs.roku.com/display/sdkdoc/External+Control+Guide
+	 */
+
+	public MSearchSDDPRequest() throws IOException {
+
+		/* create byte arrays to hold our send and response data */
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+
+		/* our M-SEARCH data as a byte array */
+		String MSEARCH = "M-SEARCH * HTTP/1.1\nHost: 239.255.255.250:1900\nMan: \"ssdp:discover\"\nST: roku:ecp\n";
+		sendData = MSEARCH.getBytes();
+
+		/* create a packet from our data destined for 239.255.255.250:1900 */
+		DatagramPacket sendPacket = null;
+		try {
+			sendPacket = new DatagramPacket(sendData, sendData.length,
+					InetAddress.getByName("239.255.255.250"), 1900);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		/* send packet to the socket we're creating */
+		DatagramSocket clientSocket = null;
+		try {
+			clientSocket = new DatagramSocket();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		clientSocket.send(sendPacket);
+
+		/* recieve response and store in our receivePacket */
+		DatagramPacket receivePacket = new DatagramPacket(receiveData,
+				receiveData.length);
+		clientSocket.receive(receivePacket);
+
+		/* get the response as a string */
+		String response = new String(receivePacket.getData());
+
+		/* print the response */
+		System.out.println(response);
+
+		/* close the socket */
+		clientSocket.close();
+	}
+}
